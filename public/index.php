269b69bab4c81f905d2f14a9cea036537f6623b3
ruub <ruubsw@outlook.com>
2020-06-24 20:12:46 +0200

    proof-of-concept feature: added a get parameter to the temporary index file feature: added some error feedback to the temporary index file

--- public/index.php
+++ public/index.php
@@ -15,10 +15,15 @@ use Monolog\Handler\StreamHandler;
 $oLogger = new Logger('remote');
 $oLogger->pushHandler(new StreamHandler('php://stdout', Logger::DEBUG));
 
-$sKey = 'KEY_HOME';
+$sKey = isset($_GET['key']) ? 'KEY_' . strtoupper($_GET['key']) : 'KEY_HOME';
 
 $oRemote = new Remote($oLogger);
 $oRemote->setHost('192.168.2.1'); // 192.168.10.36
-$oRemote->sendKey($sKey);
 
-echo "Sent the {$sKey} request.";
+try {
+    $oRemote->sendKey($sKey);
+
+    echo "Sent the {$sKey} request.";
+} catch (Exception $e) {
+    echo "Error: {$e->getMessage()}";
+}
