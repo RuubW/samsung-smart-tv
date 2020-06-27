a445cfb87fdb9ee8479ea5aebb4b3b9ffb41e876
ruub <ruubsw@outlook.com>
2020-06-27 14:59:16 +0200

    symfony-form - chore: added a separate parameters configuration file - refactor: moved the valid keys list from code to configuration

--- src/Library/Remote.php
+++ src/Library/Remote.php
@@ -54,59 +54,34 @@ class Remote
 	 */
 	private $appName = 'PHP Remote';
 
+    /**
+     * @var array
+     */
+    private $validKeys;
+
 	/**
 	 * @var array
 	 */
 	private $queue = [];
 
 	/**
-	 * @var array
-     *
-	 * This list is taken from https://github.com/Bntdumas/SamsungIPRemote/blob/master/samsungKeyCodes.txt
-	 */
-	private $aValidKeys = array(
-        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '11', '12', '4_3', '16_9', '3SPEED', 'AD', 'ADDDEL',
-        'ALT_MHP', 'ANGLE', 'ANTENA', 'ANYNET', 'ANYVIEW', 'APP_LIST', 'ASPECT', 'AUTO_ARC_ANTENNA_AIR',
-        'AUTO_ARC_ANTENNA_CABLE', 'AUTO_ARC_ANTENNA_SATELLITE', 'AUTO_ARC_ANYNET_AUTO_START',
-        'AUTO_ARC_ANYNET_MODE_OK', 'AUTO_ARC_AUTOCOLOR_FAIL', 'AUTO_ARC_AUTOCOLOR_SUCCESS', 'AUTO_ARC_C_FORCE_AGING',
-        'AUTO_ARC_CAPTION_ENG', 'AUTO_ARC_CAPTION_KOR', 'AUTO_ARC_CAPTION_OFF', 'AUTO_ARC_CAPTION_ON',
-        'AUTO_ARC_C_FORCE_AGING', 'AUTO_ARC_JACK_IDENT', 'AUTO_ARC_LNA_OFF', 'AUTO_ARC_LNA_ON',
-        'AUTO_ARC_PIP_CH_CHANGE', 'AUTO_ARC_PIP_DOUBLE', 'AUTO_ARC_PIP_LARGE', 'AUTO_ARC_PIP_LEFT_BOTTOM',
-        'AUTO_ARC_PIP_LEFT_TOP', 'AUTO_ARC_PIP_RIGHT_BOTTOM', 'AUTO_ARC_PIP_RIGHT_TOP', 'AUTO_ARC_PIP_SMALL',
-        'AUTO_ARC_PIP_SOURCE_CHANGE', 'AUTO_ARC_PIP_WIDE', 'AUTO_ARC_RESET', 'AUTO_ARC_USBJACK_INSPECT', 'AUTO_FORMAT',
-        'AUTO_PROGRAM', 'AV1', 'AV2', 'AV3', 'BACK_MHP', 'BLUE', 'BOOKMARK', 'CALLER_ID', 'CAPTION', 'CATV_MODE',
-        'CHDOWN', 'CH_LIST', 'CHUP', 'CLEAR', 'CLOCK_DISPLAY', 'COMPONENT1', 'COMPONENT2', 'CONTENTS', 'CONVERGENCE',
-        'CONVERT_AUDIO_MAINSUB', 'CUSTOM', 'CYAN', 'DEVICE_CONNECT', 'DISC_MENU', 'DMA', 'DNET', 'DNIe', 'DNSe',
-        'DOOR', 'DOWN', 'DSS_MODE', 'DTV', 'DTV_LINK', 'DTV_SIGNAL', 'DVD_MODE', 'DVI', 'DVR', 'DVR_MENU', 'DYNAMIC',
-        'ENTER', 'ENTERTAINMENT', 'ESAVING', 'EXT1', 'EXT10', 'EXT11', 'EXT12', 'EXT13', 'EXT14', 'EXT15', 'EXT16',
-        'EXT17', 'EXT18', 'EXT19', 'EXT2', 'EXT20', 'EXT21', 'EXT22', 'EXT23', 'EXT24', 'EXT25', 'EXT26', 'EXT27',
-        'EXT28', 'EXT29', 'EXT3', 'EXT30', 'EXT31', 'EXT32', 'EXT33', 'EXT34', 'EXT35', 'EXT36', 'EXT37', 'EXT38',
-        'EXT39', 'EXT4', 'EXT40', 'EXT41', 'EXT5', 'EXT6', 'EXT7', 'EXT8', 'EXT9', 'FACTORY', 'FAVCH', 'FF', 'FF_',
-        'FM_RADIO', 'GAME', 'GREEN', 'GUIDE', 'HDMI', 'HDMI1', 'HDMI2', 'HDMI3', 'HDMI4', 'HELP', 'HOME', 'ID_INPUT',
-        'ID_SETUP', 'INFO', 'INSTANT_REPLAY', 'LEFT', 'LINK', 'LIVE', 'MAGIC_BRIGHT', 'MAGIC_CHANNEL', 'MDC', 'MENU',
-        'MIC', 'MORE', 'MOVIE1', 'MS', 'MTS', 'MUTE', 'NINE_SEPERATE', 'OPEN', 'PANNEL_CHDOWN', 'PANNEL_CHUP',
-        'PANNEL_ENTER', 'PANNEL_MENU', 'PANNEL_POWER', 'PANNEL_SOURCE', 'PANNEL_VOLDOW', 'PANNEL_VOLUP', 'PANORAMA',
-        'PAUSE', 'PCMODE', 'PERPECT_FOCUS', 'PICTURE_SIZE', 'PIP_CHDOWN', 'PIP_CHUP', 'PIP_ONOFF', 'PIP_SCAN',
-        'PIP_SIZE', 'PIP_SWAP', 'PLAY', 'PLUS100', 'PMODE', 'POWER', 'POWEROFF', 'POWERON', 'PRECH', 'PRINT',
-        'PROGRAM', 'QUICK_REPLAY', 'REC', 'RED', 'REPEAT', 'RESERVED1', 'RETURN', 'REWIND', 'REWIND_', 'RIGHT',
-        'RSS', 'RSURF', 'SCALE', 'SEFFECT', 'SETUP_CLOCK_TIMER', 'SLEEP', 'SOURCE', 'SRS', 'STANDARD', 'STB_MODE',
-        'STILL_PICTURE', 'STOP', 'SUB_TITLE', 'SVIDEO1', 'SVIDEO2', 'SVIDEO3', 'TOOLS', 'TOPMENU', 'TTX_MIX',
-        'TTX_SUBFACE', 'TURBO', 'TV', 'TV_MODE', 'UP', 'VCHIP', 'VCR_MODE', 'VOLDOWN', 'VOLUP', 'WHEEL_LEFT',
-        'WHEEL_RIGHT', 'W_LINK', 'YELLOW', 'ZOOM1', 'ZOOM2', 'ZOOM_IN', 'ZOOM_MOVE', 'ZOOM_OUT'
-	);
-
-	/**
 	 * Remote constructor.
      *
-     * @param string $host
      * @param AdapterInterface $cache
-	 * @param LoggerInterface $logger
+     * @param LoggerInterface $logger
+     * @param string $host
+     * @param array $validKeys
 	 */
-	public function __construct($host, AdapterInterface $cache, LoggerInterface $logger)
-	{
+	public function __construct(
+        AdapterInterface $cache,
+        LoggerInterface $logger,
+	    string $host,
+        array $validKeys
+    ) {
+        $this->cache = $cache;
+        $this->logger = $logger;
         $this->host = $host;
-	    $this->cache = $cache;
-		$this->logger = $logger;
+        $this->validKeys = $validKeys;
 	}
 
 	/**
@@ -122,17 +97,17 @@ class Remote
 	/**
 	 * Validate a key against the list of valid keys.
      *
-	 * @param string $sKey
+	 * @param string $key
      *
 	 * @return bool
 	 */
-	private function validateKey($sKey): bool
+	private function validateKey($key): bool
     {
-        if (substr($sKey, 0, 4) == 'KEY_') {
-            $sKey = substr($sKey, 4);
+        if (substr($key, 0, 4) == 'KEY_') {
+            $key = substr($key, 4);
         }
 
-		return in_array($sKey,$this->aValidKeys);
+		return (empty($this->validKeys) || in_array($key, $this->validKeys));
 	}
 
 	/**
@@ -144,17 +119,17 @@ class Remote
 	 */
 	private function getKeypressMessage($sKey): string
 	{
-		$aMessage = array(
+		$aMessage = [
 			'method' => 'ms.remote.control',
-			'params' => array(
+			'params' => [
 				'Cmd' => 'Click',
 				'DataOfCmd' => $sKey,
 				'Option' => false,
 				'TypeOfRemote' => 'SendRemoteKey'
-			)
-		);
+			]
+		];
 
-		return json_encode($aMessage,JSON_PRETTY_PRINT);
+		return json_encode($aMessage, JSON_PRETTY_PRINT);
 	}
 
 	/**
@@ -163,16 +138,16 @@ class Remote
 	 * @param string $sKey
 	 * @param float $fDelay
 	 */
-	public function queueKey($sKey,$fDelay = 1.0): void
+	public function queueKey($sKey, $fDelay = 1.0): void
     {
         if (!$this->validateKey($sKey)) {
             throw new UnexpectedValueException("Invalid key: {$sKey}");
         }
 
-		$this->queue[] = array(
+		$this->queue[] = [
 		    'key' => $sKey,
             'delay' => $fDelay
-        );
+        ];
 	}
 
 	/**
@@ -180,7 +155,7 @@ class Remote
 	 */
 	public function clearQueue(): void
 	{
-		$this->queue = array();
+		$this->queue = [];
 	}
 
 	/**
